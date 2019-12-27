aa2055b39ee16ce298bdb6e29a1c4d3225a9e17a
sun li <sun.li@uneed.com>
2019-12-27 17:33:49 +0800

    Add unit test for message methods
    Test encoding and reading the message.

--- message/message_test.go
+++ message/message_test.go
@@ -2,19 +2,20 @@ package message
 
 import (
 	"bytes"
+	"encoding/json"
 	"github.com/google/go-cmp/cmp"
 	"testing"
 )
 
 // Test Encode and Decode message Header
-func TestHeader_Encode(t *testing.T) {
+func TestHeader_EncodeAndDecode(t *testing.T) {
 	header := Header{
 		Version:      0,
 		MessageType:  MsgTypeRequest,
 		CompressType: NoneCompress,
-		SeqID:        12345678,
+		SeqID:        1,
 		Extension:    0,
-		BodyLength:   100100,
+		BodyLength:   10,
 	}
 
 	hrd := EncodeHeader(header)
@@ -26,6 +27,51 @@ func TestHeader_Encode(t *testing.T) {
 	}
 
 	diff := cmp.Diff(header, h)
+	if diff != "" {
+		t.Fatalf(diff)
+	}
+}
+
+// Test message encode and receive
+func TestMessage_EncodeAndRecv(t *testing.T) {
+
+	// create request
+	name := "leo"
+	age := 18
+	req := Request{
+		Path:   "Student",
+		Method: "Register",
+		Args:   []interface{}{name, age},
+	}
+	reqData, err := json.Marshal(req)
+	if err != nil {
+		t.Fatalf("TestMessage_EncodeAndRecv|Marshal|Fail|%v", err)
+		return
+	}
+	// create message
+	msg := Message{
+		Header: &Header{
+			Version:      0,
+			MessageType:  MsgTypeRequest,
+			CompressType: NoneCompress,
+			SeqID:        1,
+			Extension:    0,
+			BodyLength:   uint32(len(reqData)),
+		},
+		Data: reqData,
+	}
+	// encode message
+	msgData := msg.Encode()
+
+	// test receive message
+	recvMsg, err := RecvMessage(bytes.NewReader(msgData))
+	if err != nil {
+		t.Fatalf("TestMessage_EncodeAndRecv|RecvMessage|Fail|%v", err)
+		return
+	}
+
+	// compare
+	diff := cmp.Diff(msg, *recvMsg)
 	if diff != "" {
 		t.Fatalf(diff)
 	}
